f96a509a2596491ca37517b4b19435a75c4767bf
ahmetmujkanovic-spec <ahmetmujkanovic@gmail.com>
2026-06-12 09:52:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,24 @@
-self.addEventListener("install", function(e) {
-    e.waitUntil(
-        caches.open("susara-cache").then(function(cache) {
-            return cache.addAll([
-                "./",
-                "./index.html",
-                "./manifest.json"
-            ]);
-        })
-    );
+const CACHE_NAME = "susara-hmi-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
 });
 
-self.addEventListener("fetch", function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
