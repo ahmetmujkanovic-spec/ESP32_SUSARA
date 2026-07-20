d089369b03562f36b4f6ec0f5e98653f012cfd88
ahmetmujkanovic-spec <ahmetmujkanovic@gmail.com>
2026-07-20 13:35:55 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SCADA v4 - SERVICE WORKER
 // =====================================================
 
-const CACHE_NAME = "scada-v4-cache-v17";
+const CACHE_NAME = "scada-v4-cache-v18";
 
 
 const FILES_TO_CACHE = [
