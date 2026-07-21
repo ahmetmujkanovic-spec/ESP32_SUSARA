9f4ed5524e41c57ab4eb0c15d15a0910ec7dc1d1
ahmetmujkanovic-spec <ahmetmujkanovic@gmail.com>
2026-07-21 08:54:04 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,62 +1,165 @@
+// settings.js
+// Glavne postavke SCADA v4
+
+
 const SCADA_SETTINGS = {
 
-    // Broj komora
+
+    // =========================
+    // SUŠARE
+    // =========================
+
+
     chambers: [
 
+
         {
+
             id: 1,
+
             name: "Sušara 1",
-            sensors: {
-                dry: "susara/1/suhi",
-                wet: "susara/1/vlazni",
-                delta: "susara/1/delta"
-            }
+
+
+            mqttPrefix:
+            "susara/1"
+
+
         },
 
+
+
         {
+
             id: 2,
+
             name: "Sušara 2",
-            sensors: {
-                dry: "susara/2/suhi",
-                wet: "susara/2/vlazni",
-                delta: "susara/2/delta"
-            }
+
+
+            mqttPrefix:
+            "susara/2"
+
+
         },
 
+
+
         {
+
             id: 3,
+
             name: "Sušara 3",
-            sensors: {
-                dry: "susara/3/suhi",
-                wet: "susara/3/vlazni",
-                delta: "susara/3/delta"
-            }
+
+
+            mqttPrefix:
+            "susara/3"
+
+
         }
+
+
 
     ],
 
 
-    // Granice alarma
 
-    alarms: {
 
-        maxTemperature: 65,
 
-        sensorTimeout: 120000   // 2 minute
+    // =========================
+    // MQTT
+    // =========================
+
+
+    mqtt: {
+
+
+        host:
+        "wss://TVOJ_HIVEMQ_SERVER:8884/mqtt",
+
+
+        username:
+        "USERNAME",
+
+
+        password:
+        "PASSWORD",
+
+
+
+        topics:{
+
+
+            dry:
+            "suhi",
+
+
+            wet:
+            "vlazni",
+
+
+            delta:
+            "delta",
+
+
+            status:
+            "status"
+
+
+        }
+
 
     },
 
 
-    // MQTT
 
-    mqtt: {
 
-        host: "wss://f061290ac3f24bb7a4bd389b716bddc6.s1.eu.hivemq.cloud:8884/mqtt",
 
-        username: "esp32",
 
-        password: "1234Aaaa"
+    // =========================
+    // ALARMI
+    // =========================
+
+
+    alarms:{
+
+
+        maxTemperature:
+        65,
+
+
+        offlineTimeout:
+        120000,
+
+
+        warningTemperature:
+        55
+
+
+    },
+
+
+
+
+
+
+
+    // =========================
+    // SISTEM
+    // =========================
+
+
+    system:{
+
+
+        name:
+        "SCADA Sušara V4",
+
+
+
+        refresh:
+        1000
+
 
     }
+
 
 };
