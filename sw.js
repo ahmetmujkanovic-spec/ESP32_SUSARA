bc4fb109fdab946421c348ed76e750bf39e92e75
ahmetmujkanovic-spec <ahmetmujkanovic@gmail.com>
2026-07-21 19:53:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@
 // =====================================================
 
 
-const APP_VERSION = "4.37";
+const APP_VERSION = "4.38";
 
-const CACHE_NAME = "scada-v4-cache-v37";
+const CACHE_NAME = "scada-v4-cache-v38";
 
 const FILES_TO_CACHE = [
 
