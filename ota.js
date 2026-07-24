89e21227a2cd1f73e71c782c00f591392ff6dad9
ahmetmujkanovic-spec <ahmetmujkanovic@gmail.com>
2026-07-24 22:04:01 +0200

    Create ota.js

---
+++ ota.js
@@ -0,0 +1,25 @@
+function updateFirmware()
+{
+
+    const url =
+    document.getElementById(
+        "firmwareUrl"
+    ).value.trim();
+
+
+    if(!url)
+    {
+        alert("Unesi firmware link");
+        return;
+    }
+
+
+    if(!confirm("Pokrenuti OTA update?"))
+    {
+        return;
+    }
+
+
+    sendFirmwareUpdate(url);
+
+}
