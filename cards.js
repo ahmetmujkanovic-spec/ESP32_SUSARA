0ff773964d3c6060757e6cd5ab73fc7e8cc3abd0
ahmetmujkanovic-spec <ahmetmujkanovic@gmail.com>
2026-07-24 22:03:35 +0200

    Update cards.js

--- cards.js
+++ cards.js
@@ -419,32 +419,6 @@ function updateChamberValue(
 }
 
 
-function updateFirmware()
-{
-
-    const url =
-    document.getElementById(
-        "firmwareUrl"
-    ).value.trim();
-
-
-    if(!url)
-    {
-        alert("Unesi firmware link");
-        return;
-    }
-
-
-    if(!confirm("Pokrenuti OTA update?"))
-    {
-        return;
-    }
-
-
-    sendFirmwareUpdate(url);
-
-}
-
 
 function updateLastSeen(id){
 
